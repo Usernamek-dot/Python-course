14962eacb1bde8fd1548343cd0529913de35bb01
karol pacheco <kypacheco07@misena.edu.co>
2022-10-07 21:30:35 -0500

    Activities made from module 2

---
+++ mod2/calculator.py
@@ -0,0 +1,22 @@
+print("--CALCULATOR--")
+# adition
+value =  input("Write a number: ")
+valueTwo = input("Write second number: ")
+total = int(value) + int(valueTwo)
+print("The adition is : ",  total)
+# substraction
+value =  input("Write a number: ")
+valueTwo = input("Write second number: ")
+total = int(value) - int(valueTwo)
+print("The sustraction is :" , total)
+# multiplication
+value =  input("Write a number: ")
+valueTwo = input("Write second number: ")
+total = int(value) * int(valueTwo)
+print("The multiplication is :" , total)
+# division
+value =  input("Write a number: ")
+valueTwo = input("Write second number: ")
+total = int(value) * int(valueTwo)
+print("The division is :" , total)
+
---
+++ mod2/vars.py
@@ -0,0 +1,18 @@
+from debugpy import listen
+
+
+varName="nami"
+varLastName="namisious"
+varNum=5
+piValue="16"
+varAge=14
+add = piValue + varAge
+list = ["item","item#2","item#3"]
+
+print("The name of my variable is" ,   varName,  varLastName)
+print("The age of my variable is " ,   str(varAge))
+# convert num type to string type
+print("The pi number is" ,  int(piValue) )
+print(type(piValue))
+print (list)
+
