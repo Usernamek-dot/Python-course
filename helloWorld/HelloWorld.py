7b4883365f0f975bf9cfb317141025aff76a4a1e
karol pacheco <kypacheco07@misena.edu.co>
2022-10-07 21:30:22 -0500

    Hello worlded in python

---
+++ helloWorld/HelloWorld.py
@@ -0,0 +1,8 @@
+print("Hello world")
+print (1)
+print("This is a number",1)
+print("hello world","hello wordly")
+#this is a comment
+"""this is another commnet,
+this is another commnet,
+this is another commnet"""
