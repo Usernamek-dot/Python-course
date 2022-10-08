3195a9db9a4bd6e673d814758062bdaad4b9158f
karol pacheco <kypacheco07@misena.edu.co>
2022-10-07 21:29:32 -0500

    Excercises and concepts of the first class of python

---
+++ classOne/comillas.py
@@ -0,0 +1,7 @@
+print("  ")
+#Es porque en Inglés se usan las simples y en otros idiomas las dobles
+
+
+
+# lorem ipsum example comment with keyboard shortcut
+
---
+++ classOne/vars.py
@@ -0,0 +1,14 @@
+variableOne = "first var"
+varTwo = "second var"
+floatVar = 4.0
+intVar = 5  
+iva = 0.50
+price = 3600
+total =  price * iva 
+varType = 5.6
+# print( "This is the type of varType:" ,  type(varType))
+# print (variableOne)
+# print ( "my var is :" ,   variableOne  ,"my second var",  varTwo)
+# print( "this is a float number: "  ,  floatVar)
+# print (floatVar + intVar) #adition example
+# print ("The total purchase amount is :  "  , total )
