a3eb09a60d232c4857398efa04a7db4f3ea55024
karol pacheco <kypacheco07@misena.edu.co>
2022-10-18 21:51:45 -0500

    doing video excersises

--- classFive/sets.py
+++ classFive/sets.py
@@ -1,3 +1,6 @@
+from math import prod
+
+
 group={"item1","item2","item3",1}
 group.add("item1234")
 group.remove("")
@@ -8,6 +11,13 @@ print(group)
 lista1 = set([1, 1, 2, 2])
 
 print(lista1)   # deletes repeated data
+
+setColors= {"color1","color2","color3","color4"}
+
+productsList ={"product1":300,"product2":7400,"product3":5000}
+totalProducts = sum(productsList.values())
+print (f"The total of the products is{totalProducts}")
+
 #-------------------sets
 
 
