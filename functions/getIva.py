c65bc099d9449aacdd9a180fe8dfa1811effc474
karol pacheco <kypacheco07@misena.edu.co>
2022-10-25 21:42:39 -0500

    working with functions #2

---
+++ functions/getIva.py
@@ -0,0 +1,5 @@
+def getIva(val):
+     return val * 0.19
+value=4500
+iva = getIva(value)     
+print(f"The iva amount {iva} is from {value}")
---
+++ functions/lambda.py
@@ -0,0 +1,5 @@
+#anonimous functions
+square = lambda x: x ** 2
+print(square(3)) # result: 9
+print(square(6)) # result: 12
+print(square(5)) # result: 10
