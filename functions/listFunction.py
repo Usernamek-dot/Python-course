f6033f46ed88d7948d2e66c181e89409891cc890
karol pacheco <kypacheco07@misena.edu.co>
2022-10-25 21:57:51 -0500

    looping a list

--- functions/listFunction.py
+++ functions/listFunction.py
@@ -1,6 +1,19 @@
-listItems = ["item","item2","item3"]
+
 def listFunction(list):
      for value in list:
           print(f"This is my list {list} ")
+def loopList(l):
+     for x in l:
+          print(f"value: {x}")
+     l.sort()
+     print(f"List in order: {l}  " )     
 
-listFunction(listItems)     
+listItems = ["item","item2","item3"]
+list =  ["item","item2","item3","item4"]   
+
+
+listFunction(listItems)   
+loopList(list)
+
+
+
