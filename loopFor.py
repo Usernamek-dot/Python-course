a5574d27cf338f493cf293690f60707d9655a157
karol pacheco <kypacheco07@misena.edu.co>
2022-10-25 21:00:14 -0500

    Feat / add loop and random samples

---
+++ loopFor.py
@@ -0,0 +1,8 @@
+
+num = input("Type number")
+value = 0
+for x  in num :
+     value = value + int(x)
+print(value)     
+
+# Elaborar un algoritmo que sume los dígitos de una cifra. Ejemplo: para la cifra 345 el algoritmo debe devolver 12
---
+++ loopWhile.py
@@ -0,0 +1,5 @@
+listOfMonths =["Jan","Feb","Mar","Ap"]
+month = 0
+while month < 3:
+     print ( f"We are in: {listOfMonths[month]}  ")
+     month += 1
