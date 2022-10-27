cb0e74b7b972caed6f46e50847524f9b77c40803
karol pacheco <kypacheco07@misena.edu.co>
2022-10-27 18:19:52 -0500

    Fix / delete  unnecesary folders

--- classThree/conditionals.py
+++
@@ -1,7 +0,0 @@
-products=["item1","item2","item3"]
-if len(products) > 6:
-      print(False) 
-else : 
-     print("The rigth length is ", len(products))
-# if len(products) > 6: print(False) 
-# else : print("The rigth length is ", len(products))
