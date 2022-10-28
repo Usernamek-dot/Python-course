042f89ce6c8d9c7bea8eaacae3d256778d6d756f
karol pacheco <kypacheco07@misena.edu.co>
2022-10-27 21:20:01 -0500

    Validating add operation. #calculator

--- calulator.py
+++ calulator.py
@@ -1,7 +1,7 @@
 divide = lambda  number,number2 :   number / number2   
 multiply = lambda  number,number2 :   number * number2   
 subtract = lambda  number,number2 :   number - number2   
-add = lambda  number,number2 :   number + number2 
+add = lambda  number,number2 :  print(f"Result  🎉 : {number + number2 } ")     
 def showOptions(x):
            for keys, values in dictionary.items():
                     print(f"{keys}.   {values} ")
@@ -13,11 +13,26 @@ def selectingOption():
          print("🩸Invalid data, try again.")   
          continue
       if select not in range(1,5): 
-         print("🩸Only must contain 1  character to the options.")
+         print("🩸Only must contain 1  character to the corresponding options.")
          continue
       else:
-         print("Success  🎋")      
-         break
+            if select == 1:
+               print("You are gonna add.  🔗 ")
+               try :
+                  num = int(input("Type a number:"))
+                  num2 = int(input("Type a number:"))
+                  add(num,num2)
+               except ValueError :
+                   print("🩸 Just a number.  Are you sure it`s a number ?")
+                   continue
+
+            elif select == 2:
+               print("You are gonna subtract 📷📸 ")   
+            elif select == 3:
+               print("You are gonna multiply ⚔")   
+            elif select == 4:
+               print("You are gonna divide 📲 ")   
+            break
 dictionary = {
    '1': 'Add',
    '2': 'Subtract',
@@ -25,13 +40,6 @@ dictionary = {
    '4': 'Divide'
 }
 print ("++++++++++ 🐍  CALCULATOR 🐍++++++++++")
-print(showOptions(dictionary))
+showOptions(dictionary)
 selectingOption()
-
-   
-   # option = int(input("🧩  To select an option, type the corresponding number : "))
-   # if option.string:
-   #    print(" 🩸 Incorrect option,  try again.")
-   # elif option == : 
-   #    print("El número es impar")
 
