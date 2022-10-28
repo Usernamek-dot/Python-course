1241d65d18d6c44830aab51f77e93b7d283b2383
karol pacheco <kypacheco07@misena.edu.co>
2022-10-27 20:09:30 -0500

    Feat / Validate data type in calculator

--- calulator.py
+++ calulator.py
@@ -2,13 +2,21 @@ divide = lambda  number,number2 :   number / number2
 multiply = lambda  number,number2 :   number * number2   
 subtract = lambda  number,number2 :   number - number2   
 add = lambda  number,number2 :   number + number2 
-def loopSelect(i):
-           for keys, value in dictionary.items():
-                    print(keys,value)
-def selecting():
-          selection= input("  🧩  To select an option, type the corresponding number :")
-          print(selection)
-
+def showOptions(x):
+           for keys, values in dictionary.items():
+                    print(f"{keys}.   {values} ")
+def selectingOption():
+   while True:
+      try:
+            select =  int(input("🧩  To select an option, type the corresponding number : "))
+      except ValueError:
+         print("🩸Invalid data, try again.")   
+         continue
+      except len(select) == 1:
+         print("🩸Only must contain 1 character.")
+      else:
+         print("Success  🎋")      
+         break
 dictionary = {
    '1': 'Add',
    '2': 'Subtract',
@@ -16,4 +24,13 @@ dictionary = {
    '4': 'Divide'
 }
 print ("++++++++++ 🐍  CALCULATOR 🐍++++++++++")
-print(loopSelect(dictionary))
+print(showOptions(dictionary))
+selectingOption()
+
+   
+   # option = int(input("🧩  To select an option, type the corresponding number : "))
+   # if option.string:
+   #    print(" 🩸 Incorrect option,  try again.")
+   # elif option == : 
+   #    print("El número es impar")
+
