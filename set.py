0b2ced7fe94981f98832db34386238d68ffbd7db
karol pacheco <kypacheco07@misena.edu.co>
2022-10-27 23:07:13 -0500

    copied excersise

---
+++ set.py
@@ -0,0 +1,23 @@
+# ____________________________________________________
+
+print ("SETS ✨")
+
+{} # Sets
+# Se definen con corchetes tipo llave {}.
+# Es una colección que es desordenada y no tiene índice.
+# Son utilizadas para definir una colección de datos.
+# El set es muy usado cuando no se requiere organizar los datos.
+
+ex9 = {"Tomates 🍅", "Melón 🍈" "Mango 🥭","Manzanas 🍏","Piñas 🍍","Cebollas 🧄"}
+
+print("Exercise 9")
+print(type(ex9).__name__)
+print(ex9)
+
+ex10 = {"HP","Dell","Lenovo","Apple"}
+
+print("Exercise 10")
+print(ex10)
+print(type(ex10).__name__)
+
+# ____________________________________________________
