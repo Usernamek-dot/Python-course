d5a2aea0f24c7fdb4294ddb7ca47103c6cd48d7d
karol pacheco <kypacheco07@misena.edu.co>
2022-10-27 23:08:54 -0500

    copied coordinate excersise

---
+++ coordinate.py
@@ -0,0 +1,9 @@
+locations = {
+    (35.12312,39.12312): "Medellín",
+    (324.12312,38.12312): "Tokio",
+    (324.12312,38.12312): "LA"
+} #tuple
+
+print("Exercise Locations 🌍")
+print(locations)
+print(type(locations).__name__)
