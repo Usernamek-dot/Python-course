001470ae69ef7137d995a2fcc11360a709894b38
karol pacheco <kypacheco07@misena.edu.co>
2022-10-27 21:55:02 -0500

    Validate input number # calculator

--- calculator/validateNumber.py
+++ calculator/validateNumber.py
@@ -1,8 +1,14 @@
 from add import *
-def validateNum():
+from subtract import *
+from divide import *
+from multiply import *
+def validateNum(x):
       try :
           num = int(input("Type a number:"))
           num2 = int(input("Type a number:"))
-          add(num,num2)
+          if x == 1:
+               add(num,num2)
+          elif x == 2:
+               subtract(num,num2)
       except ValueError :
            print("🩸 Just a number.  Are you sure it`s a number ?")
