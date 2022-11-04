2fc5779f0a6d0401b22ed2ac6c110c0037c265ab
karol pacheco <kypacheco07@misena.edu.co>
2022-11-03 22:19:56 -0500

    Feat / add def to loop DF

--- panda-env/src/app.py
+++ panda-env/src/app.py
@@ -13,9 +13,24 @@ df.head()
 print("Tail: it shows last 5 results")
 df.tail()
 print("Describe: It shows statistical information . It actually creates a DF")
-print.describe()
+df.describe()
 print("Sample: to choose random records")
-print.sample(2)
+df.sample(2)
 df[2:9]#range
 print("Iloc: loop DF")
 df.iloc[3]
+def clearData(name,lastname,mark1,mark2,mark3,mark4):
+     print(f"Full name:  {name.upper()} {lastname.upper()} ")
+     print(f"Grade1 {round(mark1,1)} ")
+     print(f"Grade2 {round(mark2,2)} ")
+     print(f"Grade3 {round(mark3,3)} ")
+     print(f"Grade4 {round(mark4,4)} ")
+
+for x in range(len(df)):
+     name = str(df.iloc[x]["Name"])
+     lastname = str(df.iloc[x]["Lastname"])
+     mark1 = float(df.iloc[x]["Grade1"])
+     mark2 = float(df.iloc[x]["Grade2"])
+     mark3 = float(df.iloc[x]["Grade3"])
+     mark4 = float(df.iloc[x]["Grade4"])
+     clearData(name,lastname,mark1,mark2,mark3,mark4)     
