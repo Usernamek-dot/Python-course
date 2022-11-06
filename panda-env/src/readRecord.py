ba3e570c852bc4b2544ca59c17659d0a0c5c5a72
karol pacheco <kypacheco07@misena.edu.co>
2022-11-05 19:15:05 -0500

    Fix / module files

---
+++ panda-env/src/readRecord.py
@@ -0,0 +1,16 @@
+from app import df
+def readRecord(name,lastname,mark1,mark2,mark3,mark4):
+     print(f"Full name:  {name.upper()} {lastname.upper()} ")
+     print(f"Grade1 {round(mark1,1)} ")
+     print(f"Grade2 {round(mark2,2)} ")
+     print(f"Grade3 {round(mark3,3)} ")
+     print(f"Grade4 {round(mark4,4)} ")
+
+for x in range(len(df)):
+     name = str(df.iloc[x]["Name"])
+     lastname = str(df.iloc[x]["Lastname"])
+     mark1 = float(df.iloc[x]["Note1"])
+     mark2 = float(df.iloc[x]["Note2"])
+     mark3 = float(df.iloc[x]["Note3"])
+     mark4 = float(df.iloc[x]["Note4"])
+     readRecord(name,lastname,mark1,mark2,mark3,mark4)     
