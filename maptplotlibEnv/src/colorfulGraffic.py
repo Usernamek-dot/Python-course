581b1d274297b372dc947706f4cd55c2aba61059
karol pacheco <kypacheco07@misena.edu.co>
2022-11-12 20:50:03 -0500

    Feat / add legends to diagram

--- maptplotlibEnv/src/colorfulGraffic.py
+++ maptplotlibEnv/src/colorfulGraffic.py
@@ -2,12 +2,9 @@ import matplotlib.pyplot as plt
 fig,ax = plt.subplots()
 days=['L', 'M', 'X', 'J', 'V', 'S', 'D']
 temperatures = {'Madrid':[28.5, 30.5, 31, 30, 28, 27.5, 30.5], 'Barcelona':[24.5, 25.5, 26.5, 25, 26.5, 24.5, 25]}
-ax.plot(days,temperatures['Madrid'], color="tab:purple")
-ax.plot(days,temperatures['Barcelona'], color="tab:yellow")
-ax.plot(days,temperatures['Madrid'], marker = 'o')
-ax.plot(days,temperatures['Barcelona'], marker = '^')
-ax.plot(days,temperatures['Madrid'], linestyle = 'dashed')
-ax.plot(days,temperatures['Barcelona'], linestyle = 'doted')
+ax.plot(days,temperatures['Madrid'], color="tab:purple",marker = 'o',linestyle = 'dashed',label="Madrid")
+ax.plot(days,temperatures['Barcelona'], color="tab:yellow",marker = '^',linestyle = 'doted',label="Barcelona")
 ax.set_title('Daily temperature', loc = "left", fontdict = {'fontsize':14, 'fontweight':'bold', 'color':'tab:green'})
+ax.legend(loc = 'upper right')
 plt.savefig("dispersion-diagram.png")
 plt.show()
