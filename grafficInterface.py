add54d507b02709a61e5eb7a3c5397759f96fb42
karol pacheco <kypacheco07@misena.edu.co>
2022-11-12 21:28:34 -0500

    Style / Add text color to label

--- grafficInterface.py
+++ grafficInterface.py
@@ -2,8 +2,7 @@ import tkinter
 window = tkinter.Tk()
 window.geometry("900x600")
 window.title("Diagram")
-
-labelTitle = tkinter.Label(window,text="Diagram label",font=("Poppins",24),padx=20,pady=20)
+labelTitle = tkinter.Label(window,text="Diagram label",font=("Poppins",24),padx=20,pady=20,fg="#2c3e50")
 labelTitle.pack(pady=15)
 labeltem = tkinter.Label(window,text="Item",font=("Poppins",14),padx=20,pady=20)
 labeltem.pack(pady=15)
