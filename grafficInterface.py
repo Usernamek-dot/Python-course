71d954224f0b47b071b2583c677765c72fe74978
karol pacheco <kypacheco07@misena.edu.co>
2022-11-12 21:23:48 -0500

    Feat / add entry to window

--- grafficInterface.py
+++ grafficInterface.py
@@ -2,8 +2,11 @@ import tkinter
 window = tkinter.Tk()
 window.geometry("900x600")
 window.title("Diagram")
-label_title = tkinter.Label(window,text="Diagram label",font=("Poppins",24),padx=20,pady=20)
-label_title.pack()
-label_item = tkinter.Label(window,text="Item",font=("Poppins",14),padx=20,pady=20)
-label_item.pack()
+
+labelTitle = tkinter.Label(window,text="Diagram label",font=("Poppins",24),padx=20,pady=20)
+labelTitle.pack(pady=15)
+labeltem = tkinter.Label(window,text="Item",font=("Poppins",14),padx=20,pady=20)
+labeltem.pack(pady=15)
+entryItem = tkinter.Entry(window,font=("Poppins",19))
+entryItem.pack()
 window.mainloop() # always at the end
