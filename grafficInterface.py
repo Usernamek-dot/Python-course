f99959fa15ce1e9b883736b8f30ad7b0e9bca7ae
karol pacheco <kypacheco07@misena.edu.co>
2022-11-12 21:18:29 -0500

    Feat / add label to window and style it

--- grafficInterface.py
+++ grafficInterface.py
@@ -2,4 +2,8 @@ import tkinter
 window = tkinter.Tk()
 window.geometry("900x600")
 window.title("Diagram")
+label_title = tkinter.Label(window,text="Diagram label",font=("Poppins",24),padx=20,pady=20)
+label_title.pack()
+label_item = tkinter.Label(window,text="Item",font=("Poppins",14),padx=20,pady=20)
+label_item.pack()
 window.mainloop() # always at the end
