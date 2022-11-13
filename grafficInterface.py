d83995d3ec93d3dddc04b2ca4b251a6bd4b9c519
karol pacheco <kypacheco07@misena.edu.co>
2022-11-12 21:38:10 -0500

    Feat / add 2nd label & button to window

--- grafficInterface.py
+++ grafficInterface.py
@@ -1,11 +1,22 @@
 import tkinter
 window = tkinter.Tk()
+#style window
 window.geometry("900x600")
 window.title("Diagram")
 labelTitle = tkinter.Label(window,text="Diagram label",font=("Poppins",24),padx=20,pady=20,fg="#2c3e50")
 labelTitle.pack(pady=15)
+#add label  & entry
 labeltem = tkinter.Label(window,text="Item",font=("Poppins",14),padx=20,pady=20)
 labeltem.pack(pady=15)
 entryItem = tkinter.Entry(window,font=("Poppins",19))
 entryItem.pack()
-window.mainloop() # always at the end
+#add label  & entry
+labeltem2 = tkinter.Label(window,text="Item",font=("Poppins",14),padx=10,pady=10)
+labeltem2.pack()
+entryItem2 = tkinter.Entry(window,font=("Poppins",19))
+entryItem2.pack(pady=15)
+#button
+btnSubmit = tkinter.Button(window,text="Submit",font=("Poppins",14,"bold"),bg="#e67e22")
+btnSubmit.pack()
+# always at the end
+window.mainloop() 
