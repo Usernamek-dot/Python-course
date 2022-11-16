2d533b006150ad553843eb32f4b394398465bda1
karol pacheco <kypacheco07@misena.edu.co>
2022-11-15 20:48:16 -0500

    Feat / create hospital constructor   & encapsult data

---
+++ Hospital/app.py
@@ -0,0 +1,6 @@
+class Patients:
+     def __init__(self,name,age,idd,bloodType) :
+          self.__name = name
+          self.__age = age
+          self.__idd = idd
+          self.__bloodType = bloodType
