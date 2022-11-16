5ae5d0eed2b2cdcebd17c84b7f98f72484717e6c
karol pacheco <kypacheco07@misena.edu.co>
2022-11-15 23:45:37 -0500

    Fix / add id input

--- Users/app.py
+++ Users/app.py
@@ -51,42 +51,41 @@ def searchData():
 #ttitle
 titleLabel = tkinter.Label(window, text="Users Form", font=('Verdana', 20, 'bold'), bg='#485460',fg="#95a5a6")
 titleLabel.grid(row=0, column=1, padx=20)
-
 #id
 idLabel = tkinter.Label(window, text="Type your id", font=('Verdana', 15, 'italic'), bg='#485460',fg="#bdc3c7")
-idLabel.grid(row=5, column=0, pady=20)
+idLabel.grid(row=1, column=0, pady=20)
 idEntry = tkinter.Entry(window, font=('Verdana', 15, 'italic'))
-idEntry.grid(row=5, column=1)
+idEntry.grid(row=1, column=1)
 #name
 nameLabel = tkinter.Label(window, text="Type your name", font=('Verdana', 15, 'italic'), bg='#485460',fg="#bdc3c7")
-nameLabel.grid(row=1, column=0, pady=10,padx=20)
+nameLabel.grid(row=2, column=0, pady=10,padx=20)
 nameEntry = tkinter.Entry(window, font=('Verdana', 15, 'italic'))
-nameEntry.grid(row=1, column=1)
+nameEntry.grid(row=2, column=1)
 #age
 ageLabel = tkinter.Label(window, text="Type your age", font=('Verdana', 15, 'italic'), bg='#485460',fg="#bdc3c7")
-ageLabel.grid(row=2, column=0, pady=20)
+ageLabel.grid(row=3, column=0, pady=20)
 ageEntry = tkinter.Entry(window, font=('Verdana', 15, 'italic'))
-ageEntry.grid(row=2, column=1)
+ageEntry.grid(row=3, column=1)
 #country
 countryLabel = tkinter.Label(window, text="Type your country", font=('Verdana', 15, 'italic'), bg='#485460',fg="#bdc3c7")
-countryLabel.grid(row=3, column=0, pady=20)
+countryLabel.grid(row=4, column=0, pady=20)
 countryEntry = tkinter.Entry(window, font=('Verdana', 15, 'italic'))
-countryEntry.grid(row=3, column=1)
+countryEntry.grid(row=4, column=1)
 #phone
 phoneLabel = tkinter.Label(window, text="Type your phone", font=('Verdana', 15, 'italic'), bg='#485460',fg="#bdc3c7")
-phoneLabel.grid(row=4, column=0, pady=20)
+phoneLabel.grid(row=5, column=0, pady=20)
 phoneEntry = tkinter.Entry(window, font=('Verdana', 15, 'italic'))
-phoneEntry.grid(row=4, column=1)
+phoneEntry.grid(row=5, column=1)
 #date
 dateLabel = tkinter.Label(window, text="Type your date", font=('Verdana', 15, 'italic'), bg='#485460',fg="#bdc3c7")
-dateLabel.grid(row=5, column=0, pady=20)
+dateLabel.grid(row=6, column=0, pady=20)
 dateEntry = tkinter.Entry(window, font=('Verdana', 15, 'italic'))
-dateEntry.grid(row=5, column=1)
-#search
-searchLabel = tkinter.Label(window, text="Type your search", font=('Verdana', 15, 'italic'), bg='#485460',fg="#bdc3c7")
-searchLabel.grid(row=6, column=0, pady=20)
-searchEntry = tkinter.Entry(window, font=('Verdana', 15, 'italic'))
-searchEntry.grid(row=6, column=1)
+dateEntry.grid(row=6, column=1)
+# #search
+# searchLabel = tkinter.Label(window, text="Type your search", font=('Verdana', 15, 'italic'), bg='#485460',fg="#bdc3c7")
+# searchLabel.grid(row=7, column=0, pady=20)
+# searchEntry = tkinter.Entry(window, font=('Verdana', 15, 'italic'))
+# searchEntry.grid(row=7, column=1)
 #buttons
 saveBtn = tkinter.Button(window, text="SAVE", font=('Verdana', 14, 'italic'), bg='#34495e', fg='#FFFFFF', command=validateData)
 saveBtn.grid(row=7, column=1, pady=20, sticky="E")
