62bbffd8880e15a02c94548af983a6546c4d77b9
karol pacheco <kypacheco07@misena.edu.co>
2022-11-15 19:32:02 -0500

    Feat / create hotel class and constructor #hotel

---
+++ hotel.py
@@ -0,0 +1,6 @@
+class Hotel:
+     def __init__(self,numRooms,parkinLot,spa,insurance) :
+          self.numRooms = numRooms
+          self.spa = True
+          self.parkinLot = parkinLot
+          self.insurance = insurance
