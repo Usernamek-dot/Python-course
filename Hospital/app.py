b8aaafd85d76a1e7acd7423bfebb7c12414febac
karol pacheco <kypacheco07@misena.edu.co>
2022-11-15 20:51:57 -0500

    Feat / create gets , sets , dels #hospital

--- Hospital/app.py
+++ Hospital/app.py
@@ -4,3 +4,20 @@ class Patients:
           self.__age = age
           self.__idd = idd
           self.__bloodType = bloodType
+          #gets
+     def getName (self):
+          return self.__name
+     def getAge (self):
+          return self.__age
+     def getId (self):
+          return self.__idd
+     def getBloodType (self):
+          return self.__bloodType
+          #sets
+     def setName(self,name):
+          self.__name = name
+     def setAge(self,age):
+          self.__age = age
+          #deletes
+     def delBloodType(self):
+          del self.__bloodType
