585f49b293f1bed3c00724ca7500fbe59b14c635
karol pacheco <kypacheco07@misena.edu.co>
2022-11-15 19:36:12 -0500

    Feat / encapsulate params with get #hotel

--- hotel.py
+++ hotel.py
@@ -1,6 +1,14 @@
 class Hotel:
      def __init__(self,numRooms,parkinLot,spa,insurance) :
-          self.numRooms = numRooms
-          self.spa = True
-          self.parkinLot = parkinLot
-          self.insurance = insurance
+          self.__numRooms = numRooms
+          self.__spa = True
+          self.__parkingLot = parkinLot
+          self.__insurance = insurance
+     def getNRooms(self):
+          return self.__numRooms
+     def getSpa(self):
+          return self.__spa
+     def getParkingLot(self):
+          return self.__parkingLot
+     def getInsurance(self):
+          return self.__insurance
