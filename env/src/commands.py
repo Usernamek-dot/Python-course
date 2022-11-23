7c13f19f7d5e26634e7c3ebadb33a9da2fce89b5
karol pacheco <kypacheco07@misena.edu.co>
2022-11-22 20:59:02 -0500

    Fix / delete envs

--- env/src/commands.py
+++
@@ -1,9 +0,0 @@
-  #to activate env -----> source ./activate  (first open scripts folder terminal)
-
-  # to create env -----> python -m venv env
-
-  #ls  ----->  to check files on folder
-
-  #pip list  ----> to check dependencies
-
-  #deactivate ---> just type the word as seen
