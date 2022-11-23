8706a352b2fd567e4ba4d08928c7472af4ce47ce
karol pacheco <kypacheco07@misena.edu.co>
2022-11-22 21:01:19 -0500

    Fix / delete envs

--- panda-env/src/app.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-# def useDF():
-file= "./Notes.xlsx"
-df = pd.read_excel(file,sheet_name="Sheet1")
--- panda-env/src/order.py
+++
@@ -1,8 +0,0 @@
-from app import df
-df.reindex(columns=["Name","Lastname","Note1","Note2","Note3","Note4"])
-df["Note1"]=round(df["Note1"],1)
-df["Note2"]=round(df["Note2"],1)
-df["Note3"]=round(df["Note3"],1)
-df["Note4"]=round(df["Note4"],1)
-df["Average"] =round((df['Note1'] + df['Note2'] + df['Note3'] + df['Note4'])/4, 1)
-print(df)
